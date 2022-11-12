fbf8e2b18281c9493d822396adeb3b417827f0c6
Derman_e <ko.heesung01@gmail.com>
2022-11-12 18:01:11 +0900

    v 1.0  - When OP Player pickups an item, checks for compress-able items and compresses the items into a block. Doesn't count the item picked up, so you have to drop the other item to auto-compress the items in your inventory.

--- src/main/java/com/k5na/autoorecompression/AutoOreCompression.java
+++ src/main/java/com/k5na/autoorecompression/AutoOreCompression.java
@@ -5,7 +5,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,7 +16,10 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
 import java.util.Map;
+
+import static org.bukkit.entity.EntityType.PLAYER;
 
 public final class AutoOreCompression extends JavaPlugin implements Listener {
 
@@ -38,22 +40,8 @@ public final class AutoOreCompression extends JavaPlugin implements Listener {
         Bukkit.getConsoleSender().sendMessage(message);
     }
 
-    public boolean checkItem(Player player, Material material, int count) {
-        Map<Integer, ? extends ItemStack> items = player.getInventory().all(material);
-        int found = 0;
-
-        for (ItemStack stack : items.values()) {
-            found += stack.getAmount();
-            console("[DEBUG] " + stack + " found in checking!");
-        }
-
-        if (count > found) {
-            console("[DEBUG] found less than count");
-            return false;
-        } else {
-            console("[DEBUG] found more than count");
-            return true;
-        }
+    public boolean invEmpty(Player player) {
+        return Arrays.asList(player.getInventory().getStorageContents()).contains(null);
     }
 
     public boolean removeItem(Player player, Material material, int count) {
@@ -62,31 +50,15 @@ public final class AutoOreCompression extends JavaPlugin implements Listener {
 
         for (ItemStack stack : ore.values()) {
             found += stack.getAmount();
-            console("[DEBUG] " + stack + " found in removing!");
         }
 
         if (count > found) {
-            console("[DEBUG] found less than count");
             return false;
         }
 
-        for (Integer index : ore.keySet()) {
-            ItemStack stack = ore.get(index);
-
-            int removed = Math.min(count, stack.getAmount());
-            count -= removed;
-
-            if (stack.getAmount() == removed) {
-                player.getInventory().setItem(index, null);
-            }
-
-            if (count <= 0) {
-                break;
-            }
-        }
+        player.getInventory().removeItemAnySlot(new ItemStack(material, count));
 
         player.updateInventory();
-        console("[DEBUG] Inventory Updated");
         return true;
     }
 
@@ -95,10 +67,11 @@ public final class AutoOreCompression extends JavaPlugin implements Listener {
         pManager.registerEvents(this, this);
         console(ChatColor.YELLOW + "onPlayerPickupItemEvent" + ChatColor.WHITE + " is now enabled!");
 
-        ShapelessRecipe quartz = new ShapelessRecipe(new NamespacedKey(this, NamespacedKey.MINECRAFT), new ItemStack(Material.QUARTZ, 4)).addIngredient(Material.QUARTZ_BLOCK);
-        console(ChatColor.YELLOW + "ShapelessRecipe" + ChatColor.WHITE + " is now enabled!");
+        ShapelessRecipe quartz = new ShapelessRecipe(new NamespacedKey(this, NamespacedKey.MINECRAFT), new ItemStack(Material.QUARTZ, 4))
+                .addIngredient(Material.QUARTZ_BLOCK);
 
         Bukkit.addRecipe(quartz);
+        console(ChatColor.YELLOW + "ShapelessRecipe" + ChatColor.WHITE + " is now enabled!");
 
         console(ChatColor.YELLOW + getFullName() + ChatColor.WHITE + " is now enabled!");
 
@@ -114,51 +87,66 @@ public final class AutoOreCompression extends JavaPlugin implements Listener {
     public void onPlayerPickupItemEvent(EntityPickupItemEvent event) {
         Entity entity = event.getEntity();
 
-        if (entity.getType() == EntityType.PLAYER) {
+        if (entity.getType() == PLAYER) {
             Player player = (Player) entity;
             Inventory inventory = player.getInventory();
 
             if (player.isOp()) {
-                if (inventory.contains(Material.AIR)) {
-                    if (checkItem(player, Material.COAL, 9)) {
-                        removeItem(player, Material.COAL, 9);
+                if (invEmpty(player)) {
+                    if (removeItem(player, Material.COAL, 9)) {
                         inventory.addItem(new ItemStack(Material.COAL_BLOCK));
-                    } else if (checkItem(player, Material.RAW_COPPER, 9)) {
-                        removeItem(player, Material.RAW_COPPER, 9);
+                    }
+
+                    if (removeItem(player, Material.RAW_COPPER, 9)) {
                         inventory.addItem(new ItemStack(Material.RAW_COPPER_BLOCK));
-                    } else if (checkItem(player, Material.COPPER_INGOT, 9)) {
-                        removeItem(player, Material.COPPER_INGOT, 9);
+                    }
+
+                    if (removeItem(player, Material.COPPER_INGOT, 9)) {
                         inventory.addItem(new ItemStack(Material.COPPER_BLOCK));
-                    } else if (checkItem(player, Material.RAW_IRON, 9)) {
-                        removeItem(player, Material.RAW_IRON, 9);
+                    }
+
+                    if (removeItem(player, Material.RAW_IRON, 9)) {
                         inventory.addItem(new ItemStack(Material.RAW_IRON_BLOCK));
-                    } else if (checkItem(player, Material.IRON_NUGGET, 9)) {
-                        removeItem(player, Material.IRON_NUGGET, 9);
+                    }
+
+                    if (removeItem(player, Material.IRON_NUGGET, 9)) {
                         inventory.addItem(new ItemStack(Material.IRON_INGOT));
-                    } else if (checkItem(player, Material.IRON_INGOT, 9)) {
-                        removeItem(player, Material.IRON_INGOT, 9);
+                    }
+
+                    if (removeItem(player, Material.IRON_INGOT, 9)) {
                         inventory.addItem(new ItemStack(Material.IRON_BLOCK));
-                    } else if (checkItem(player, Material.LAPIS_LAZULI, 9)) {
-                        removeItem(player, Material.LAPIS_LAZULI, 9);
+                    }
+
+                    if (removeItem(player, Material.LAPIS_LAZULI, 9)) {
                         inventory.addItem(new ItemStack(Material.LAPIS_BLOCK));
-                    } else if (checkItem(player, Material.RAW_GOLD, 9)) {
-                        removeItem(player, Material.RAW_GOLD, 9);
+                    }
+
+                    if (removeItem(player, Material.RAW_GOLD, 9)) {
                         inventory.addItem(new ItemStack(Material.RAW_GOLD_BLOCK));
-                    } else if (checkItem(player, Material.GOLD_NUGGET, 9)) {
-                        removeItem(player, Material.GOLD_NUGGET, 9);
+                    }
+
+                    if (removeItem(player, Material.GOLD_NUGGET, 9)) {
                         inventory.addItem(new ItemStack(Material.GOLD_INGOT));
-                    } else if (checkItem(player, Material.GOLD_INGOT, 9)) {
-                        removeItem(player, Material.GOLD_INGOT, 9);
+                    }
+
+                    if (removeItem(player, Material.GOLD_INGOT, 9)) {
                         inventory.addItem(new ItemStack(Material.GOLD_BLOCK));
-                    } else if (checkItem(player, Material.REDSTONE, 9)) {
-                        removeItem(player, Material.REDSTONE, 9);
+                    }
+
+                    if (removeItem(player, Material.REDSTONE, 9)) {
                         inventory.addItem(new ItemStack(Material.REDSTONE_BLOCK));
-                    } else if (checkItem(player, Material.DIAMOND, 9)) {
-                        removeItem(player, Material.DIAMOND, 9);
+                    }
+
+                    if (removeItem(player, Material.DIAMOND, 9)) {
                         inventory.addItem(new ItemStack(Material.DIAMOND_BLOCK));
-                    } else if (checkItem(player, Material.EMERALD, 9)) {
-                        removeItem(player, Material.EMERALD, 9);
+                    }
+
+                    if (removeItem(player, Material.EMERALD, 9)) {
                         inventory.addItem(new ItemStack(Material.EMERALD_BLOCK));
+                    }
+
+                    if (removeItem(player, Material.WHEAT, 9)) {
+                        inventory.addItem(new ItemStack(Material.HAY_BLOCK));
                     }
 
                     player.updateInventory();
